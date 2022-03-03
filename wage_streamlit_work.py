386c1b21fa6faf2bd79b016581e9628f64e39885
tussiiiii <ikumi.tsushima@gmail.com>
2022-03-03 23:32:13 +0900

    csv file path mod. commit

--- wage_streamlit_work.py
+++ wage_streamlit_work.py
@@ -6,13 +6,13 @@ import plotly.express as px
 
 st.title('日本の賃金データダッシュボード')
 
-df_jp_ind = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_全国_全産業.csv', encoding='shift_jis')
-df_jp_category = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_全国_大分類.csv.', encoding='shift_jis')
-df_pref_ind = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv', encoding='shift_jis')
+df_jp_ind = pd.read_csv('csv_data/雇用_医療福祉_一人当たり賃金_全国_全産業.csv', encoding='shift_jis')
+df_jp_category = pd.read_csv('csv_data/雇用_医療福祉_一人当たり賃金_全国_大分類.csv.', encoding='shift_jis')
+df_pref_ind = pd.read_csv('csv_data/雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv', encoding='shift_jis')
 
 st.header('■2019年：一人当たり平均賃金のヒートマップ')
 
-jp_lat_lon = pd.read_csv('./pref_lat_lon.csv')
+jp_lat_lon = pd.read_csv('pref_lat_lon.csv')
 jp_lat_lon = jp_lat_lon.rename(columns={'pref_name': '都道府県名'})
 
 df_pref_map = df_pref_ind[(df_pref_ind['年齢'] == '年齢計') & (df_pref_ind['集計年'] == 2019)]
