0db2827ab96dec4c457328b636020cf31f31acaf
tussiiiii <ikumi.tsushima@gmail.com>
2022-03-04 23:06:26 +0900

    pathlib mod. commit

--- wage_streamlit_work.py
+++ wage_streamlit_work.py
@@ -2,13 +2,19 @@ import pandas as pd
 import streamlit as st
 import pydeck as pdk
 import plotly.express as px
+from pathlib import Path 
 
 
 st.title('日本の賃金データダッシュボード')
 
-df_jp_ind = pd.read_csv('csv_data/雇用_医療福祉_一人当たり賃金_全国_全産業.csv', encoding='shift_jis')
-df_jp_category = pd.read_csv('csv_data/雇用_医療福祉_一人当たり賃金_全国_大分類.csv.', encoding='shift_jis')
-df_pref_ind = pd.read_csv('csv_data/雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv', encoding='shift_jis')
+""" CSVファイルのパス(カレントディレクトリ) """
+
+p_jp_ind = Path('./csv_data/雇用_医療福祉_一人当たり賃金_全国_全産業.csv')
+df_jp_ind = pd.read_csv(p_jp_ind.absolute(), encoding='shift_jis')
+p_jp_category = Path('./csv_data/雇用_医療福祉_一人当たり賃金_全国_大分類.csv')
+df_jp_category = pd.read_csv(p_jp_ind.absolute(), encoding='shift_jis')
+p_pref_ind = Path('./csv_data/雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv')
+df_pref_ind = pd.read_csv(p_pref_ind.absolute(), encoding='shift_jis')
 
 st.header('■2019年：一人当たり平均賃金のヒートマップ')
 
